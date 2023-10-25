523629575266204dbde3794cc8693241c0b240ba
anasrulloh22 <146051233+anasrulloh22@users.noreply.github.com>
2023-10-25 15:39:09 +0700

    Add files via upload

---
+++ tugas no.1-2.Pekan 5.py
@@ -0,0 +1,35 @@
+# Tugas 1 Pertemuan 5
+motor=["mio","matic","110","hitam","roda dua"]
+print(motor)
+motor.append("7juta")
+motor.append("CBS")
+print(motor)
+motor.insert(2,"yamaha")
+print(motor)
+
+# Tugas 2 Pertemuan 5
+print("""
+    *** PILIH OPERASI ***
+    1. HITUNG PERSEGI
+    2. HITUNG LINGKARAN
+    3. HITUNG SEGITIGA
+    """)
+pilihan = input("masukan pilihan: ")
+match pilihan:
+    case "1":
+        s = int(input ("masukkan sisi:"))
+        persegi = s*s
+        print("luas persegi",persegi)
+    case "2":
+        phi = 3.14
+        r = int(input ("masukkan jari:"))
+        lingkaran = phi*r*r
+        print("luas lingkaran",lingkaran)
+    case "3":
+        l = 1/2
+        a = int(input ("masukkan alas:"))
+        t = int(input ("masukkan tinggi:"))
+        segitiga = l*a*t
+        print("luas segitiga",segitiga)
+    case _:
+        print("pilihan tidak ada")
