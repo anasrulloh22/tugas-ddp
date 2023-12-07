2bb48a1fd2ef25a5762e0325ec9d1d7b97e5d2dc
anasrulloh22 <146051233+anasrulloh22@users.noreply.github.com>
2023-12-07 14:30:47 +0700

    Add files via upload

---
+++ hasil.py
@@ -0,0 +1,26 @@
+from bangun_datar import *
+from aritmatika import *
+
+# Hasil Bangun_Datar
+persegi(5)
+persegi_panjang(5, 2)
+segitiga_sama_sisi(15, 25)
+jajar_genjang(30,25,10)
+lingkaran(8)
+belah_ketupat(8,7,2)
+layang_layang(6,8,4,2)
+
+# Hasil Aritmatika
+penjumlahan(10,4)
+pengurangan(15,5)
+perkalian(20,5)
+pembagian(80, 10)
+modulus(12, 8)
+eksponen(9,7)
+akar(6,4)
+pangkat(20,5)
+median(20,10)
+taksiran(20,8)
+pembulatan(8,2)
+
+
